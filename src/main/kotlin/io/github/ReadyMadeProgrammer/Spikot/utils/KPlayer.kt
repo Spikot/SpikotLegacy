eac43005fa1e186b17d4d6234d39b6c50b6b3fe1
ReadyMadeProgrammer <jun2620354@naver.com>
2018-08-08 18:03:42 +0900

    Refactor whole code

--- src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/utils/KPlayer.kt
+++ src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/utils/KPlayer.kt
@@ -1,4 +1,5 @@
 @file:Suppress("unused")
+
 package io.github.ReadyMadeProgrammer.Spikot.utils
 
 import com.google.common.collect.HashBiMap
@@ -13,8 +14,8 @@ import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.plugin.Plugin
 import java.util.*
 
-private val variableMap = mutableMapOf<UUID,MutableMap<String,Any>>()
-private val legacySupport = HashBiMap.create<String,UUID>()
+private val variableMap = mutableMapOf<UUID, MutableMap<String, Any>>()
+private val legacySupport = HashBiMap.create<String, UUID>()
 
 @Suppress("UNCHECKED_CAST")
 operator fun <T> Player.get(key: String): T = variableMap[id]?.get(key) as T
@@ -22,18 +23,22 @@ operator fun <T> Player.get(key: String): T = variableMap[id]?.get(key) as T
 @Suppress("IMPLICIT_CAST_TO_ANY")
 operator fun <T> Player.set(key: String, value: T) = if (value != null) variableMap[id]?.set(key, value as Any) else variableMap.remove(id)
 
+fun Player.has(key: String) = variableMap[id]!!.containsKey(key)
+
 fun Player.remove(key: String) = variableMap[id]!!.remove(key)
 
-object KPlayerListener: Listener {
-    @EventHandler(priority= EventPriority.LOWEST) fun onJoin(e: PlayerJoinEvent){
+object KPlayerListener : Listener {
+    @EventHandler(priority = EventPriority.LOWEST)
+    fun onJoin(e: PlayerJoinEvent) {
         variableMap[e.player.id] = mutableMapOf()
     }
 
-    @EventHandler(priority= EventPriority.LOWEST) fun onQuit(e: PlayerQuitEvent){
+    @EventHandler(priority = EventPriority.LOWEST)
+    fun onQuit(e: PlayerQuitEvent) {
         variableMap.remove(e.player.id)
     }
 
-    fun start(plugin: Plugin){
+    fun start(plugin: Plugin) {
         Bukkit.getOnlinePlayers().forEach { variableMap[it.id] = mutableMapOf() }
         plugin.subscribe(this)
     }
--- src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/utils/MessageUtil.kt
+++ src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/utils/MessageUtil.kt
@@ -2,5 +2,5 @@ package io.github.ReadyMadeProgrammer.Spikot.utils
 
 import org.bukkit.ChatColor
 
-operator fun ChatColor.plus(message: String) = this.toString()+message
-operator fun ChatColor.plus(color: ChatColor) = this.toString()+color.toString()
+operator fun ChatColor.plus(message: String) = this.toString() + message
+operator fun ChatColor.plus(color: ChatColor) = this.toString() + color.toString()
