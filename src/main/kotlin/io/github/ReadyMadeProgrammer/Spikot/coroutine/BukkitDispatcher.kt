9d5144ad49a2e795a85f319c65cc6e9ffa2f3e46
ReadyMadeProgrammer <jun2620354@kaist.ac.kr>
2019-07-19 23:01:29 +0900

    Rewrite whole coroutine codes Remove code from skedule

--- src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/coroutine/BukkitDispatcher.kt
+++ src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/coroutine/BukkitDispatcher.kt
@@ -1,38 +1,133 @@
 package io.github.ReadyMadeProgrammer.Spikot.coroutine
 
+import io.github.ReadyMadeProgrammer.Spikot.spikotPlugin
 import kotlinx.coroutines.*
 import org.bukkit.Bukkit
 import org.bukkit.plugin.Plugin
-import org.bukkit.scheduler.BukkitTask
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.coroutineContext
 
-@InternalCoroutinesApi
-class BukkitDispatcher(private val plugin: Plugin, private val synchronizationType: SynchronizationType) : CoroutineDispatcher(), Delay {
+@UseExperimental(InternalCoroutinesApi::class)
+@PublishedApi
+internal class BukkitDispatcher(@PublishedApi internal val plugin: Plugin, private val isSync: Boolean) : CoroutineDispatcher(), Delay {
     override fun dispatch(context: CoroutineContext, block: Runnable) {
-        if (!context.isActive) {
+        if (!context.isActive)
             return
-        }
-        if (Bukkit.isPrimaryThread() && synchronizationType == SynchronizationType.SYNC) {
-            block.run()
-        } else {
-            if (synchronizationType == SynchronizationType.SYNC) {
-                Bukkit.getScheduler().runTask(plugin, block)
-            } else {
-                Bukkit.getScheduler().runTaskAsynchronously(plugin, block)
+        schedule(0, block::run)
+    }
+
+    @UseExperimental(ExperimentalCoroutinesApi::class)
+    override fun isDispatchNeeded(context: CoroutineContext): Boolean {
+        return !isSync || !Bukkit.isPrimaryThread()
+    }
+
+    @UseExperimental(InternalCoroutinesApi::class)
+    override suspend fun delay(time: Long) {
+        super.delay(time)
+    }
+
+    override fun invokeOnTimeout(timeMillis: Long, block: Runnable): DisposableHandle {
+        val task = schedule(timeMillis) { block.run() }
+        return object : DisposableHandle {
+            override fun dispose() {
+                task.cancel()
             }
         }
     }
 
-    @ExperimentalCoroutinesApi
+    @UseExperimental(ExperimentalCoroutinesApi::class)
     override fun scheduleResumeAfterDelay(timeMillis: Long, continuation: CancellableContinuation<Unit>) {
-        val task = run<BukkitTask> {
-            if (synchronizationType == SynchronizationType.SYNC) {
-                Bukkit.getScheduler().runTaskLater(plugin, Runnable { continuation.apply { resumeUndispatched(Unit) } }, timeMillis / 50)
-            } else {
-                Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, Runnable { continuation.apply { resumeUndispatched(Unit) } }, timeMillis / 50)
-
-            }
-        }
+        val task = schedule(timeMillis) { with(continuation) { resumeUndispatched(Unit) } }
         continuation.invokeOnCancellation { task.cancel() }
     }
+
+    private fun schedule(timeMillis: Long, runnable: () -> Unit) =
+            if (isSync)
+                Bukkit.getScheduler().runTaskLater(plugin, runnable, timeMillis / 50)
+            else
+                Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, runnable, timeMillis / 50)
+}
+
+suspend inline fun delayTick(tick: Long) {
+    delay(tick * 50)
+}
+
+private val pluginSpecifiedScope = ConcurrentHashMap<String, Pair<CoroutineScope, CoroutineScope>>()
+private fun getScope(plugin: Plugin): Pair<CoroutineScope, CoroutineScope> {
+    return pluginSpecifiedScope.getOrPut(plugin.name) {
+        Pair(
+                CoroutineScope(CoroutineName("${plugin.name}-sync") + BukkitDispatcher(plugin, true) + SupervisorJob()),
+                CoroutineScope(CoroutineName("${plugin.name}-async") + BukkitDispatcher(plugin, false) + SupervisorJob())
+        )
+    }
+}
+
+val Plugin.syncScope: CoroutineScope
+    get() {
+        val pair = getScope(this)
+        return pair.first
+    }
+
+val Plugin.asyncScope: CoroutineScope
+    get() {
+        val pair = getScope(this)
+        return pair.second
+    }
+
+val Plugin.syncDispatcher: CoroutineDispatcher
+    get() = syncScope.coroutineContext[ContinuationInterceptor.Key] as CoroutineDispatcher
+val Plugin.asyncDispatcher: CoroutineDispatcher
+    get() = asyncScope.coroutineContext[ContinuationInterceptor.Key] as CoroutineDispatcher
+
+inline fun Plugin.launchSync(
+        context: CoroutineContext = EmptyCoroutineContext,
+        start: CoroutineStart = CoroutineStart.DEFAULT,
+        crossinline block: suspend CoroutineScope.() -> Unit
+): Job {
+    return syncScope.launch(context, start) { block() }
+}
+
+inline fun Plugin.launchAsync(
+        context: CoroutineContext = EmptyCoroutineContext,
+        start: CoroutineStart = CoroutineStart.DEFAULT,
+        crossinline block: suspend CoroutineScope.() -> Unit
+): Job {
+    return asyncScope.launch(context, start) { block() }
+}
+
+suspend inline fun <T> switchSync(
+        context: CoroutineContext = EmptyCoroutineContext,
+        crossinline block: suspend CoroutineScope.() -> T
+): T {
+    val interceptor = coroutineContext[ContinuationInterceptor]
+    if (interceptor as? BukkitDispatcher == null)
+        throw IllegalStateException("switchSync can only called from BukkitDispatcher")
+    return withContext(context + interceptor.plugin.syncDispatcher) { block() }
+}
+
+suspend inline fun <T> switchAsync(
+        context: CoroutineContext = EmptyCoroutineContext,
+        crossinline block: suspend CoroutineScope.() -> T): T {
+    val interceptor = coroutineContext[ContinuationInterceptor]
+    if (interceptor as? BukkitDispatcher == null)
+        throw IllegalStateException("switchAsync can only called from BukkitDispatcher")
+    return withContext(context + interceptor.plugin.asyncDispatcher) { block() }
+}
+
+inline fun <T> Plugin.async(
+        context: CoroutineContext = EmptyCoroutineContext,
+        start: CoroutineStart = CoroutineStart.DEFAULT,
+        crossinline block: suspend CoroutineScope.() -> T): Deferred<T> {
+    return asyncScope.async(context, start) { block() }
+}
+
+suspend inline fun <T> bukkitAsync(
+        context: CoroutineContext = EmptyCoroutineContext,
+        start: CoroutineStart = CoroutineStart.DEFAULT,
+        crossinline block: suspend CoroutineScope.() -> T): Deferred<T> {
+    val plugin = (coroutineContext[ContinuationInterceptor] as? BukkitDispatcher)?.plugin ?: spikotPlugin
+    return plugin.asyncScope.async(context, start) { block() }
 }
--- src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/coroutine/SynchronizationType.kt
+++
@@ -1,5 +0,0 @@
-package io.github.ReadyMadeProgrammer.Spikot.coroutine
-
-enum class SynchronizationType {
-    SYNC, ASYNC
-}
