5ff32e37ef0d3f36ec8faa3b22e1dcce14e008d6
ReadyMadeProgrammer <jun2620354@kaist.ac.kr>
2019-07-20 17:05:11 +0900

    Support async packet listen

--- src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/packet/PacketHandler.kt
+++ src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/packet/PacketHandler.kt
@@ -8,6 +8,7 @@ import kotlin.reflect.KClass
 @Target(AnnotationTarget.FUNCTION)
 annotation class PacketHandler(
         vararg val packets: KClass<out AbstractPacket>,
+        val async: Boolean = false,
         val priority: ListenerPriority = ListenerPriority.NORMAL,
         val ignoreCancelled: Boolean = true
 )
--- src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/packet/PacketListenerManager.kt
+++ src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/packet/PacketListenerManager.kt
@@ -2,6 +2,7 @@ package io.github.ReadyMadeProgrammer.Spikot.packet
 
 import com.comphenix.protocol.PacketType
 import com.comphenix.protocol.ProtocolLibrary
+import com.comphenix.protocol.events.ListenerOptions
 import com.comphenix.protocol.events.PacketAdapter
 import com.comphenix.protocol.events.PacketEvent
 import io.github.ReadyMadeProgrammer.Spikot.module.AbstractModule
@@ -13,29 +14,33 @@ import io.github.ReadyMadeProgrammer.Spikot.module.ModuleManager
 object PacketListenerManager : AbstractModule() {
     override fun onEnable() {
         val manager = ProtocolLibrary.getProtocolManager()
-        ModuleManager.instances.forEach { (holder, instance) ->
-            for (method in instance.javaClass.declaredMethods) {
-                val annotation = method.getDeclaredAnnotation(PacketHandler::class.java) ?: continue
-                manager.addPacketListener(object : PacketAdapter(
-                        holder.plugin,
-                        annotation.priority,
-                        annotation.packets.map {
-                            it.java.getDeclaredField("TYPE").get(null) as PacketType
-                        }
-                ) {
-                    override fun onPacketReceiving(event: PacketEvent) {
-                        if (!event.isCancelled || !annotation.ignoreCancelled) {
-                            method.invoke(instance, event)
-                        }
-                    }
+        val asyncManager =
+                ModuleManager.instances.forEach { (holder, instance) ->
+                    for (method in instance.javaClass.declaredMethods) {
+                        val annotation = method.getDeclaredAnnotation(PacketHandler::class.java) ?: continue
+                        val options = if (annotation.async) arrayOf(ListenerOptions.ASYNC) else arrayOf()
+                        val packetAdapter = object : PacketAdapter(
+                                holder.plugin,
+                                annotation.priority,
+                                annotation.packets.map {
+                                    it.java.getDeclaredField("TYPE").get(null) as PacketType
+                                },
+                                *options
+                        ) {
+                            override fun onPacketReceiving(event: PacketEvent) {
+                                if (!event.isCancelled || !annotation.ignoreCancelled) {
+                                    method.invoke(instance, event)
+                                }
+                            }
 
-                    override fun onPacketSending(event: PacketEvent) {
-                        if (!event.isCancelled || !annotation.ignoreCancelled) {
-                            method.invoke(instance, event)
+                            override fun onPacketSending(event: PacketEvent) {
+                                if (!event.isCancelled || !annotation.ignoreCancelled) {
+                                    method.invoke(instance, event)
+                                }
+                            }
                         }
+                        manager.addPacketListener(packetAdapter)
                     }
-                })
-            }
-        }
+                }
     }
 }
