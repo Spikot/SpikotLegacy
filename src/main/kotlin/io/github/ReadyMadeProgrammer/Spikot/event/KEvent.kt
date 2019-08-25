be6eaf3f8ef80fad0760ac3b38945736ea1cbfb3
ReadyMadeProgrammer <jun2620354@kaist.ac.kr>
2019-08-25 23:59:42 +0900

    Optimize cancel

--- src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/event/KEvent.kt
+++ src/main/kotlin/io/github/ReadyMadeProgrammer/Spikot/event/KEvent.kt
@@ -3,23 +3,31 @@ package io.github.ReadyMadeProgrammer.Spikot.event
 import org.bukkit.Bukkit
 import org.bukkit.event.Event
 import org.bukkit.event.EventPriority
+import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.RegisteredListener
 
 @Suppress("unused")
 inline fun <reified T : Event> Plugin.subscribe(
         priority: EventPriority = EventPriority.NORMAL,
         ignoreCancelled: Boolean = true,
         crossinline executable: SelfCancellableEventListener.(T) -> Unit) {
-    val listener = object : SelfCancellableEventListener {}
-    Bukkit.getPluginManager()
-            .registerEvent(
-                    T::class.java,
+    val handler = T::class.java.getDeclaredMethod("getHandlerList").invoke(null) as HandlerList
+    val listener = object : SelfCancellableEventListener {
+        override fun cancel() {
+            handler.unregister(this)
+        }
+    }
+    handler.register(
+            RegisteredListener(
                     listener,
+                    { _, event -> listener.executable(event as T) },
                     priority,
-                    { cancel, event -> (cancel as SelfCancellableEventListener).executable(event as T) },
                     this,
-                    ignoreCancelled)
+                    ignoreCancelled
+            )
+    )
 }
 
 fun Plugin.subscribe(listener: Listener) = Bukkit.getPluginManager().registerEvents(listener, this)
