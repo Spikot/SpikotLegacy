14107b2eae4dca4dd05ffebb8c32fd91a95d21ba
ReadyMadeProgrammer <jun2620354@kaist.ac.kr>
2019-12-01 23:50:36 +0900

    Fix test

--- src/test/kotlin/io/github/ReadyMadeProgrammer/VersionAdapterTest.kt
+++ src/test/kotlin/io/github/ReadyMadeProgrammer/VersionAdapterTest.kt
@@ -1,24 +1,22 @@
 package io.github.ReadyMadeProgrammer
 
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 import kotlin.test.assertTrue
 
 class VersionAdapterTest {
-    private val regex = Pattern.compile("((\\(\\[)\\S+)?(\\w+,\\w+)+(\\S+(\\)]))?")
-    val full = "(1.0.0-beta+exp.sha.5114f85 , 1.0.0-beta]"
-    val frontHalf = "[1.0.0-beta+exp.sha.5114f85"
-    val endHalf = "1.0.0-beta+exp.sha.5114f85)"
+    private val regex = Pattern.compile("(([(\\[])(\\S+))?(\\s*~\\s*)((\\S+)([)\\]]))?")
+    val full = "(1.0.0-beta+exp.sha.5114f85 ~ 1.0.0-beta]"
+    val frontHalf = "[1.0.0-beta+exp.sha.5114f85 ~"
+    val endHalf = "~1.0.0-beta+exp.sha.5114f85)"
     val all = "all"
 
-    lateinit var fullMatcher: Matcher
-    lateinit var frontHalfMatcher: Matcher
-    lateinit var endHalfMatcher: Matcher
+    val fullMatcher: Matcher
+    val frontHalfMatcher: Matcher
+    val endHalfMatcher: Matcher
 
-    @BeforeAll
-    fun beforeAll() {
+    init {
         fullMatcher = regex.matcher(full)
         frontHalfMatcher = regex.matcher(frontHalf)
         endHalfMatcher = regex.matcher(endHalf)
