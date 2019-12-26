3b905eef911dbe44e728b0bee008a096b1603b7f
ReadyMadeProgrammer <jun2620354@kaist.ac.kr>
2019-12-27 00:00:49 +0900

    Add extensions for Option

--- src/main/kotlin/kr/heartpattern/spikot/misc/Option.kt
+++ src/main/kotlin/kr/heartpattern/spikot/misc/Option.kt
@@ -5,3 +5,13 @@ sealed class Option<out T>
 class Just<T>(val value: T) : Option<T>()
 
 object None : Option<Nothing>()
+
+fun <T> Option<T>.getOrNull(): T? = (this as? Just<T>)?.value
+
+fun <T> Option<T>.getOrElse(default: ()->T): T = (this as? Just<T>)?.value?:default()
+
+val <T: Any> T?.option: Option<T>
+    get() = if(this == null) None else Just(this)
+
+val <T> T.just: Option<T>
+    get() = Just(this)
