f2fb79cf37defe716cde3a11ef0e6e983ea5c641
ReadyMadeProgrammer <jun2620354@kaist.ac.kr>
2019-12-27 00:33:20 +0900

    Fix Option

--- src/main/kotlin/kr/heartpattern/spikot/misc/Option.kt
+++ src/main/kotlin/kr/heartpattern/spikot/misc/Option.kt
@@ -16,3 +16,30 @@ class Just<T>(val value: T) : Option<T>()
  * Optional type without value
  */
 object None : Option<Nothing>()
+
+/**
+ * Extract value from option or null
+ * @receiver Option to extract
+ * @return Value of option or null
+ */
+fun <T> Option<T>.getOrNull(): T? = (this as? Just<T>)?.value
+
+/**
+ * Extract value from option or default value
+ * @receiver Option to extract
+ * @param default Default value supplier
+ * @receiver Value of option or default value
+ */
+fun <T> Option<T>.getOrElse(default: () -> T): T = (this as? Just<T>)?.value ?: default()
+
+/**
+ * Create Option from nullable value
+ */
+val <T : Any> T?.option: Option<T>
+    get() = if (this == null) None else Just(this)
+
+/**
+ * Create Just from value
+ */
+val <T> T.just: Option<T>
+    get() = Just(this)
