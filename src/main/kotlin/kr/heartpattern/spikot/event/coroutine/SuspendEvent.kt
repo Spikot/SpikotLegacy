2d4ed1779ad9817f960a8173ef7e90ec7a6226c4
HeartPattern <jun2620354@kaist.ac.kr>
2020-01-25 19:03:45 +0900

    Refactor suspend event functions to use hot event executor.

---
+++ src/main/kotlin/kr/heartpattern/spikot/event/coroutine/SuspendEvent.kt
@@ -0,0 +1,33 @@
+package kr.heartpattern.spikot.event.coroutine
+
+import kr.heartpattern.spikot.SpikotPlugin
+import kr.heartpattern.spikot.event.hot.HotEventManager
+import kr.heartpattern.spikot.misc.Just
+import kr.heartpattern.spikot.misc.Option
+import org.bukkit.event.Event
+import org.bukkit.event.EventPriority
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+/**
+ * Register new event listener and suspend until callback return Just
+ * @receiver SpikotPlugin which manage event listener
+ * @param E Type of event
+ * @param R Return type
+ * @param priority EventPriority
+ * @param ignoreCancelled Whether ignore event cancellation
+ * @param callback Callback to listen event and produce result
+ */
+suspend inline fun <reified E : Event, R> SpikotPlugin.suspendListenEvent(
+    priority: EventPriority,
+    ignoreCancelled: Boolean = false,
+    crossinline callback: (E) -> Option<R>
+): R = suspendCoroutine { continuation ->
+    HotEventManager.register<E>(priority, ignoreCancelled) { event, self ->
+        val result = callback(event)
+        if (result is Just) {
+            self.unregister()
+            continuation.resume(result.value)
+        }
+    }
+}
