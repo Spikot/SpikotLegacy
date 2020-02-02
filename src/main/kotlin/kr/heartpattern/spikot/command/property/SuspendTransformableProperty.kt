ab278d340a02ca50d30e9a91dcae1c7e97cbc962
HeartPattern <jun2620354@kaist.ac.kr>
2020-02-02 16:19:40 +0900

    Apply apache license 2.0

--- src/main/kotlin/kr/heartpattern/spikot/command/property/SuspendTransformableProperty.kt
+++ src/main/kotlin/kr/heartpattern/spikot/command/property/SuspendTransformableProperty.kt
@@ -1,20 +1,35 @@
+/*
+ * Copyright 2020 HeartPattern
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package kr.heartpattern.spikot.command.property
 
 import kr.heartpattern.spikot.command.CommandContext
-import javax.xml.transform.Transformer
 
 /**
  * Transformable property delegate which can use suspend function in lambda.
  * @param T Type of property
  */
-interface SuspendTransformableProperty<T>: TransformableProperty<T>{
+interface SuspendTransformableProperty<T> : TransformableProperty<T> {
     /**
      * Transform this property with suspend transformer
      * @param R Destination type
      * @param transformer suspend lambda that transform this property
      * @return Transformed property
      */
-    fun <R> suspendTransform(transformer: suspend TransformerContext.(T)->R): SuspendTransformableProperty<R>
+    fun <R> suspendTransform(transformer: suspend TransformerContext.(T) -> R): SuspendTransformableProperty<R>
 
     /**
      * Validate this property with suspend validator
